e28fa279996b1cb7a9e5ef4562e88b08eed89b1b
vladimer <gshatakishvili@gmail.com>
2023-02-01 00:07:17 +0400

    Fix issue when working with buyers/providers

--- base/views/general_views.py
+++ base/views/general_views.py
@@ -1,7 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import ListAPIView
 
-from base.filters import ProvidersFilter
+from base.filters import ProvidersFilter, BuyersFilter
 from base.models import User
 from base.serializers import UserSerializer
 
@@ -11,4 +11,11 @@ class ProvidersAPIView(ListAPIView):
     queryset = User.objects.filter(is_provider=True)
     filter_backends = [DjangoFilterBackend]
     filterset_class = ProvidersFilter
+
+
+class BuyersAPIView(ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.filter(is_buyer=True)
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = BuyersFilter
 
