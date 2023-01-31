0c52d6bd9a3b950d4d8aa1dfed9d4770197dfc9e
vladimer <gshatakishvili@gmail.com>
2023-02-01 00:06:10 +0400

    Fix issue in general_urls.py

--- base/urls/general_urls.py
+++ base/urls/general_urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from base.views.general_views import ProvidersAPIView
+from base.views.general_views import ProvidersAPIView, BuyersAPIView
 
 urlpatterns = [
     path('buyers/', BuyersAPIView.as_view(), name="buyers"),
