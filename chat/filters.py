b464086edc528bb69e2f070cca05e04ca3b072f5
vladimer <gshatakishvili@gmail.com>
2023-02-08 23:31:35 +0400

    Support searching users id field

--- chat/filters.py
+++ chat/filters.py
@@ -3,6 +3,7 @@ from django.db.models import Q
 
 
 class UsersFilter(filters.FilterSet):
+    id = filters.NumberFilter(field_name='id')
     query = filters.CharFilter(method='filter_query')
 
     def filter_query(self, queryset, name, value):
